7989378ab65d23a4a9e4a9851febd89ff40c133f
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-05-13 17:34:15 -0500

    Create basic views for product and user

---
+++ views/producto/destacados.php
@@ -0,0 +1,35 @@
+<h1 class="text-center mt-10 font-bold text-2xl uppercase">Productos destacados</h1>
+<div class="grid grid-cols-4 mt-5 gap-5 px-4">
+    <div class="flex flex-col items-center gap-1.5">
+        <img src="./assets/img/PS5%20-%20Spiderman.png" alt="Spider-Man: Miles Morales" class="h-80">
+        <h2 class="font-bold mt-2">Spider-Man: Miles Morales</h2>
+        <p class="font-semibold text-blue-900">$1,200</p>
+        <a href=""
+           class="border bg-blue-700 text-center text-white rounded-md w-24 self-center cursor-pointer hover:bg-blue-800">Comprar</a>
+    </div>
+    <div class="flex flex-col items-center gap-1.5">
+        <img src="./assets/img/Xbox%20-%20Red%20Dead%20Redemption%20II.png" alt="Red Dead Redemption 2"
+             class="h-80">
+        <h2 class="font-bold mt-2">Red Dead Redemption 2</h2>
+        <p class="font-semibold text-blue-900">$600</p>
+        <a href=""
+           class="border bg-blue-700 text-center text-white rounded-md w-24 self-center cursor-pointer hover:bg-blue-800">Comprar</a>
+    </div>
+    <div class="flex flex-col items-center gap-1.5">
+        <img src="./assets/img/Switch%20-%20The%20Legend%20of%20Zelda%20Breath%20of%20the%20Wild.png"
+             alt="The Legend of Zelda: Breath of the Wild"
+             class="h-80">
+        <h2 class="font-bold mt-2 text-sm">The Legend of Zelda: Breath of the Wild</h2>
+        <p class="font-semibold text-blue-900">$1,000</p>
+        <a href=""
+           class="border bg-blue-700 text-center text-white rounded-md w-24 self-center cursor-pointer hover:bg-blue-800">Comprar</a>
+    </div>
+    <div class="flex flex-col items-center gap-1.5">
+        <img src="./assets/img/3DS%20-%20Animal%20Crossing%20New%20Leaf.png" alt="Animal Crossing: New Leaf"
+             class="h-80">
+        <h2 class="font-bold mt-2">Animal Crossing: New Leaf</h2>
+        <p class="font-semibold text-blue-900">$500</p>
+        <a href=""
+           class="border bg-blue-700 text-center text-white rounded-md w-24 self-center cursor-pointer hover:bg-blue-800">Comprar</a>
+    </div>
+</div>
