4acc51071c9dc64108c45dbd9b081fe1edcc80b8
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-05-16 11:49:31 -0500

    Add register in UsuarioController and add ErrorController

---
+++ controllers/ErrorController.php
@@ -0,0 +1,11 @@
+<?php
+
+class ErrorController
+{
+    public function index()
+    {
+        echo "<h1 class='text-center font-bold text-3xl mt-10'>Lo siento, la paginas que buscas no existe.</h1>";
+    }
+
+
+}
--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'models/Usuario.php';
 
 class UsuarioController
 {
@@ -12,7 +13,23 @@ class UsuarioController
         require_once 'views/usuario/registro.php';
     }
 
-    public function guardarUsuario() {
-
+    public function guardar()
+    {
+        if (isset($_POST)) {
+            $usuario = new Usuario();
+            $usuario->setNombre($_POST['nombre']);
+            $usuario->setApellidos($_POST['apellidos']);
+            $usuario->setEmail($_POST['email']);
+            $usuario->setPassword($_POST['password']);
+            $guardar = $usuario->guardar();
+            if ($guardar) {
+                $_SESSION['registro'] = "exitoso";
+            } else {
+                $_SESSION['registro'] = "fallido";
+            }
+        } else {
+            $_SESSION['registro'] = "fallido";
+        }
+        header("Location:" . BASE_URL . 'usuario/registro');
     }
 }
