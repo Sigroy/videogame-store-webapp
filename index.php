e6b20fdf7338b4fc466581fad94fd21e7647c6ac
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-05-16 11:50:23 -0500

    Change URL links for the project

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
+ob_start();
+session_start();
 require_once 'autoload.php';
+require_once 'config/db.php';
+require_once 'config/parameters.php';
 require_once 'views/layout/nav.php';
 require_once 'views/layout/sidebar.php';
 
+function mostrarError()
+{
+    $error = new ErrorController();
+    $error->index();
+}
+
 if (isset($_GET['controller'])) {
     $nombre_controlador = $_GET['controller'] . 'Controller';
+} elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+    $nombre_controlador = DEFAULT_CONTROLLER;
 } else {
-    echo "Lo siento, la página que buscas no existe.";
+    mostrarError();
     exit;
 }
 
@@ -16,11 +28,16 @@ if (class_exists($nombre_controlador)) {
     if (isset($_GET['action']) && method_exists($controlador, $_GET['action'])) {
         $action = $_GET['action'];
         $controlador->$action();
+    } elseif (!isset($_GET['controller']) && !isset($_GET['action'])) {
+        $action_default = DEFAULT_ACTION;
+        $controlador->$action_default();
     } else {
-        echo "Lo siento, la página que buscas no existe.";
+        mostrarError();
     }
 } else {
-    echo "Lo siento, la página que buscas no existe.";
+    mostrarError();
 }
 
 require_once 'views/layout/footer.php';
+
+ob_end_flush();
