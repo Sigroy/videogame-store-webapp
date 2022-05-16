4c5cb6871ba3ef91172c363ae2ec11b2b5f6ba9e
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-05-16 11:51:14 -0500

    Start model entity for User and start databse connection

---
+++ models/Usuario.php
@@ -0,0 +1,139 @@
+<?php
+
+class Usuario {
+    private $id_usuario;
+    private $nombre;
+    private $apellidos;
+    private $email;
+    private $password;
+    private $rol;
+    private $imagen;
+    private $db;
+
+    public function __construct() {
+        $this->db = Database::connect();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdUsuario()
+    {
+        return $this->id_usuario;
+    }
+
+    /**
+     * @param mixed $id_usuario
+     */
+    public function setIdUsuario($id_usuario): void
+    {
+        $this->id_usuario = $id_usuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre): void
+    {
+        $this->nombre = $this->db->real_escape_string($nombre);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * @param mixed $apellidos
+     */
+    public function setApellidos($apellidos): void
+    {
+        $this->apellidos = $this->db->real_escape_string($apellidos);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $this->db->real_escape_string($email);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = password_hash($this->db->real_escape_string($password), PASSWORD_BCRYPT, ['cost' => 4]);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRol()
+    {
+        return $this->rol;
+    }
+
+    /**
+     * @param mixed $rol
+     */
+    public function setRol($rol): void
+    {
+        $this->rol = $rol;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImagen()
+    {
+        return $this->imagen;
+    }
+
+    /**
+     * @param mixed $imagen
+     */
+    public function setImagen($imagen): void
+    {
+        $this->imagen = $imagen;
+    }
+
+    public function guardar() {
+        $sql = "INSERT INTO usuario VALUES(NULL, '{$this->getNombre()}', '{$this->getApellidos()}', '{$this->getEmail()}', '{$this->getPassword()}', 'user', null)";
+        $registro = $this->db->query($sql);
+
+        $resultado = false;
+        if($registro) {
+            $resultado = true;
+        }
+        return $resultado;
+    }
+}
