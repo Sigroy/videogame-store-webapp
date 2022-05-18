04052597af825bdadc597773ca04944e153e7787
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-05-18 17:43:17 -0500

    Create helper function to check if a user is Admin and to show categories

--- helpers/utils.php
+++ helpers/utils.php
@@ -11,4 +11,20 @@ class Utils
 
         return $nombre;
     }
+
+    public static function isAdmin()
+    {
+        if (!isset($_SESSION['admin'])) {
+            header("Location:" . BASE_URL);
+        } else {
+            return true;
+        }
+    }
+
+    public static function mostrarCategorias() {
+        require_once 'models/Categoria.php';
+        $categoria = new Categoria();
+        $categorias = $categoria->getAll();
+        return $categorias;
+    }
 }
