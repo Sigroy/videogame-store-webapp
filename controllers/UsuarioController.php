a3f78050d4b74f866173149ad1557245d8bbe345
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-05-18 10:46:28 -0500

    Add login and logout methods for the user controller

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -16,20 +16,65 @@ class UsuarioController
     public function guardar()
     {
         if (isset($_POST)) {
-            $usuario = new Usuario();
-            $usuario->setNombre($_POST['nombre']);
-            $usuario->setApellidos($_POST['apellidos']);
-            $usuario->setEmail($_POST['email']);
-            $usuario->setPassword($_POST['password']);
-            $guardar = $usuario->guardar();
-            if ($guardar) {
-                $_SESSION['registro'] = "exitoso";
+            $nombre = $_POST['nombre'] ?? false;
+            $apellidos = $_POST['apellidos'] ?? false;
+            $email = $_POST['email'] ?? false;
+            $password = $_POST['password'] ?? false;
+
+            if ($nombre && $apellidos && $email && $password) {
+                $usuario = new Usuario();
+                $usuario->setNombre($nombre);
+                $usuario->setApellidos($apellidos);
+                $usuario->setEmail($email);
+                $usuario->setPassword($password);
+
+                $guardar = $usuario->guardar();
+                if ($guardar) {
+                    $_SESSION['registro'] = "exitoso";
+                } else {
+                    $_SESSION['registro'] = "fallido";
+                }
             } else {
                 $_SESSION['registro'] = "fallido";
             }
         } else {
             $_SESSION['registro'] = "fallido";
         }
-        header("Location:" . BASE_URL . 'usuario/registro');
+        header("Location:" . BASE_URL . '/usuario/registro');
+    }
+
+    public function login()
+    {
+        if (isset($_POST)) {
+            $usuario = new Usuario();
+            $usuario->setEmail($_POST['email']);
+            $usuario->setPassword($_POST['password']);
+
+            $identidad = $usuario->login();
+
+            if ($identidad && is_object($identidad)) {
+                $_SESSION['identidad'] = $identidad;
+
+                if ($identidad->rol === 'admin') {
+                    $_SESSION['admin'] = true;
+                }
+            } else {
+                $_SESSION['error_login'] = "Identificación fallida.";
+            }
+        }
+
+        header("Location:".BASE_URL);
+    }
+
+    public function logout() {
+        if (isset($_SESSION['identidad'])) {
+            unset($_SESSION['identidad']);
+        }
+
+        if (isset($_SESSION['admin'])) {
+            unset($_SESSION['admin']);
+        }
+
+        header("Location:". BASE_URL);
     }
 }
