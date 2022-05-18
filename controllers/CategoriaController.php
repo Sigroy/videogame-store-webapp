2be553af8cc92818a6d3999bebd8c31fce99d721
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-05-18 17:42:40 -0500

    Add functions for category controller

--- controllers/CategoriaController.php
+++ controllers/CategoriaController.php
@@ -1,9 +1,30 @@
 <?php
+require_once "models/Categoria.php";
 
 class CategoriaController
 {
     public function index()
     {
-        echo "Controlador Categoría, acción index";
+        Utils::isAdmin();
+        $categoria = new Categoria();
+        $categorias = $categoria->getAll();
+        require_once 'views/categoria/index.php';
+    }
+
+    public function crear()
+    {
+        Utils::isAdmin();
+        require_once 'views/categoria/crear.php';
+    }
+
+    public function guardar()
+    {
+        Utils::isAdmin();
+        if (isset($_POST) && isset($_POST['nombre'])) {
+            $categoria = new Categoria();
+            $categoria->setNombre($_POST['nombre']);
+            $guardar = $categoria->guardar();
+        }
+        header("Location:" . BASE_URL . "/categoria/index");
     }
 }
