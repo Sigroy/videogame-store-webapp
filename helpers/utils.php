df67dbb698166184aa4aa39b262b52f14f72d2ed
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-05-18 10:45:14 -0500

    Create helpers directory for utility classes

---
+++ helpers/utils.php
@@ -0,0 +1,14 @@
+<?php
+
+class Utils
+{
+    public static function eliminarSesion($nombre)
+    {
+        if (isset($_SESSION[$nombre])) {
+            $_SESSION[$nombre] = null;
+            unset($_SESSION[$nombre]);
+        }
+
+        return $nombre;
+    }
+}
