6605831df2c26c510c4ca40b795afb15b56eca6d
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-05-19 03:10:09 -0500

    Try mang

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '173fc2e4a55ab3827c5407075a8a77af258ec948',
+        'name' => 'vendor_name/proyecto-ds',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/proyecto-ds' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '173fc2e4a55ab3827c5407075a8a77af258ec948',
+            'dev_requirement' => false,
+        ),
+    ),
+);
