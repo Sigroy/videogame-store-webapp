c02b3735bceb2279cb931f587fc6afb2568726d1
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-05-19 03:06:38 -0500

    Add more tests

--- config/db.php
+++ config/db.php
@@ -6,17 +6,17 @@ class Database
     {
 
 
-//Get Heroku ClearDB connection information
-        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $cleardb_server = $cleardb_url["host"];
-        $cleardb_username = $cleardb_url["user"];
-        $cleardb_password = $cleardb_url["pass"];
-        $cleardb_db = substr($cleardb_url["path"], 1);
-        $active_group = 'default';
-        $query_builder = TRUE;
-// Connect to DB
+        /*//Get Heroku ClearDB connection information
+                $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+                $cleardb_server = $cleardb_url["host"];
+                $cleardb_username = $cleardb_url["user"];
+                $cleardb_password = $cleardb_url["pass"];
+                $cleardb_db = substr($cleardb_url["path"], 1);
+                $active_group = 'default';
+                $query_builder = TRUE;
+        // Connect to DB*/
 
-        $db = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+        $db = new mysqli('localhost', 'sig', '1234', 'tienda_videojuegos');
         $db->query("SET NAMES 'utf8'");
         return $db;
     }
