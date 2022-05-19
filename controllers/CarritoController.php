4ad6125617da1000120bab94b2f7b031c5ce04be
Sigroy <sigfredoolmedo6c17@gmail.com>
2022-05-19 01:34:09 -0500

    Create controller for the shopping cart

--- controllers/CarritoController.php
+++ controllers/CarritoController.php
@@ -1,14 +1,60 @@
 <?php
+require_once "models/Producto.php";
 
 class CarritoController
 {
     public function index()
     {
-        echo "Controlador Usuario, acción index";
+        $carrito = $_SESSION['carrito'];
+        require_once "./views/carrito/index.php";
     }
 
-    public function registro()
+    public function agregar()
     {
-        require_once 'views/usuario/registro.php';
+        if (isset($_GET['id_producto'])) {
+            $id_producto = $_GET['id_producto'];
+        } else {
+            header("Location:" . BASE_URL);
+        }
+
+        if (isset($_SESSION['carrito'])) {
+            $contador = 0;
+            foreach ($_SESSION['carrito'] as $indice => $elemento) {
+                if ($elemento['id_producto'] == $id_producto) {
+                    $_SESSION['carrito'][$indice]['unidades']++;
+                    $contador++;
+                }
+            }
+
+        }
+
+        if (!isset($contador) || $contador == 0) {
+            $producto = new Producto();
+            $producto->setIdProducto($id_producto);
+            $producto = $producto->getOne();
+
+            if (is_object($producto)) {
+                $_SESSION['carrito'][] = array(
+                    "id_producto" => $producto->id_producto,
+                    "precio" => $producto->precio,
+                    "unidades" => 1,
+                    "producto" => $producto
+                );
+            }
+        }
+
+
+        header("Location:" . BASE_URL . "/carrito/index");
+    }
+
+    public function limpiar()
+    {
+
+    }
+
+    public function eliminarTodo()
+    {
+        unset($_SESSION['carrito']);
+        header("Location:" . BASE_URL . "/carrito/index");
     }
 }
